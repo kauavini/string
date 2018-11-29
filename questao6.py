c4499402a2e4fb055ef20ed863b74a64f201bdeb
kauavini <45333104+kauavini@users.noreply.github.com>
2018-11-29 16:33:03 -0300

    Update questao6.py

--- questao6.py
+++ questao6.py
@@ -1,27 +1,7 @@
 data = str(input('Data de nascimento: '))
 mes = data[3:5]
-if mes == '01':
-  mes = 'Janeiro'
-if mes == '02':
-  mes = 'Fevereiro'
-if mes == '03':
-  mes = 'Março'
-if mes == '04':
-  mes == 'Abril'
-if mes == '05':
-  mes = 'Maio'
-if mes == '06':
-  mes = 'Junho'
-if mes == '7':
-  mes = 'Julho'
-if mes == '8':
-  mes = 'Agosto'
-if mes == '9':
-  mes = 'Setembro'
-if mes == '10':
-  mes = 'Outubro'
-if mes == '11':
-  mes = 'Novembro'
-if mes == '12':
-  mes = 'Dezembro'
+if mes[0] == '0'
+print(mes)
+meses = 'Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro'
+meses = meses.split()
 print('Você nasceu em {} de {} de {}'.format(data[0:2], mes, data[6:10]))
